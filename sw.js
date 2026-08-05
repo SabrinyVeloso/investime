8b5fd615e95f220d141f42327076af699d53e828
Sabriny Kauany de Miranda Mendes Veloso <sabriny_veloso@educadventista.org>
2026-08-05 23:24:21 +0000

    Deploy to GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'investime-cache-v1';
-const APP_SHELL = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'investime-cache-v2';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
